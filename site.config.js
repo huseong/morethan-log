3cfbbb826e53f8aa7e455a3daee1fdf2cb8e9a15
huseong2 <huseong734@gmail.com>
2023-05-02 17:21:40 +0900

    test

--- site.config.js
+++ site.config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    name: "Huseong Lee",
+    image: "/avatar.jpg",
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
+    email: "huseong734@gmail.com",
     linkedin: "morethanmin",
     github: "morethanmin",
     instagram: "",
