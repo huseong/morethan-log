1fa395258af85296ba3a6c2385c44752ccf5d44c
huseong2 <huseong734@gmail.com>
2023-12-30 13:07:18 +0900

    inactive mermaid

--- src/containers/Detail/hooks/useMermaidEffect.ts
+++ src/containers/Detail/hooks/useMermaidEffect.ts
@@ -2,25 +2,25 @@ import { useEffect } from "react"
 import mermaid from "mermaid"
 
 const useMermaidEffect = () => {
-  useEffect(() => {
-    mermaid.initialize({
-      startOnLoad: true,
-    })
-    if (!document) return
-    const elements: HTMLCollectionOf<Element> =
-      document.getElementsByClassName("language-mermaid")
-    if (!elements) return
-
-    for (let i = 0; i < elements.length; i++) {
-      mermaid.render(
-        "mermaid" + i,
-        elements[i].textContent || "",
-        (svgCode: string) => {
-          elements[i].innerHTML = svgCode
-        }
-      )
-    }
-  }, [])
+  // useEffect(() => {
+  //   mermaid.initialize({
+  //     startOnLoad: true,
+  //   })
+  //   if (!document) return
+  //   const elements: HTMLCollectionOf<Element> =
+  //     document.getElementsByClassName("language-mermaid")
+  //   if (!elements) return
+  //
+  //   for (let i = 0; i < elements.length; i++) {
+  //     mermaid.render(
+  //       "mermaid" + i,
+  //       elements[i].textContent || "",
+  //       (svgCode: string) => {
+  //         elements[i].innerHTML = svgCode
+  //       }
+  //     )
+  //   }
+  // }, [])
 
   return
 }
--- src/containers/Detail/index.tsx
+++ src/containers/Detail/index.tsx
@@ -13,7 +13,7 @@ type Props = {
 }
 
 const Detail: React.FC<Props> = ({ blockMap, data }) => {
-  useMermaidEffect()
+  // useMermaidEffect()
 
   return (
     <div>
