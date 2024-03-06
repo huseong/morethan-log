d311050464a8e6bad5973edab02b63c5b2f79bd0
huseong2 <huseong734@gmail.com>
2024-03-06 17:03:27 +0900

    ttt

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,8 +14,8 @@ import {getCategoryHierarchyFromPosts} from "@libs/utils/notion/getCategoryMapFr
 export async function getStaticProps() {
   try {
     const posts = await getPosts()
+    console.log(posts)
     const filteredPost = filterPosts(posts)
-    console.log(filteredPost)
     const tags = getAllSelectItemsFromPosts("tags", filteredPost)
     const categories = getCategoryHierarchyFromPosts(filteredPost)
 
